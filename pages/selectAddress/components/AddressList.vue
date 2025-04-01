f3a247c7dd30185c408d80ec0921a7a1e12461f3
YHLC-HS76AWIFIZ\Admin <614850049@qq.com>
2025-04-01 20:26:14 +0800

    优化组件样式，调整代码格式，添加新的空状态图片，更新加载更多组件的属性和样式，修复地址列表的功能问题。

--- pages/selectAddress/components/AddressList.vue
+++ pages/selectAddress/components/AddressList.vue
@@ -1,165 +1,291 @@
 <template>
-	<scroll-view class="list-wrapper" scroll-y>
-		<template v-if="(type === 1 && supplyList.length > 0 ) || (type === 2) && unloadList.length > 0">
-			<view class="location-list">
-				<template v-if="type === 1" >
-					<my-empty v-if="isInit1 && supplyList.length === 0"/>
-					<template v-else>
-						<view class="location" :class="{ 'my-border-bottom' : index < supplyList.length - 1 }" v-for="(item,index) in supplyList" :key="item.Id" @click="select(item)">
-							<view class="name">
-								<view class="icon">
-									<uv-image src="/static/images/location.png" width="32rpx" height="32rpx" :duration="0" />
-								</view>
-								{{ item.Name }}
-							</view>
-							<view class="address">{{ item.Province || '' }}{{ item.City || '' }}{{ item.Address || '' }}</view>
-							<view class="dis" v-if="getDis({ latitude: item.Latitude, longitude: item.Logitude })">距您 {{ getDis({ latitude: item.Latitude, longitude: item.Logitude }) }} km</view>
-						</view>
-					</template>
-				</template>
-				<template v-if="type === 2" >
-					<view class="location" :class="{ 'my-border-bottom' : index < unloadList.length - 1 }" v-for="(item,index) in unloadList" :key="item.Id" @click="select(item)">
-						<view class="name">
-							<view class="icon">
-								<uv-image src="/static/images/location.png" width="32rpx" height="32rpx" :duration="0" />
-							</view>
-							{{ item.Placename }}
-						</view>
-						<view class="address">{{ item.Province || '' }}{{ item.City || '' }}{{ item.District || '' }}{{ item.Address || '' }}</view>
-						<view class="dis" v-if="getDis({ latitude: item.Latitude, longitude: item.Logitude })">距您 {{ getDis({ latitude: item.Latitude, longitude: item.Logitude }) }} km</view>
-					</view>
-				</template>
-			</view>
-			<uv-load-more status="nomore" color="#B0BECC" line-color="#B0BECC" />
-		</template>
-		<my-empty v-else :text="type === '1' ? '暂无装货地' : '暂无卸货地'"/>
-	</scroll-view>
+  <view
+    class="list-wrapper"
+    :style="{ transform: `translateY(${translateY})` }"
+  >
+    <template v-if="type === 1">
+      <my-empty
+        v-if="isSupplyInit && supplyList.length === 0"
+        img="/static/images/empty/address1.png"
+        text="与生产企业签约后方可派单"
+        showButton
+        buttonText="查看生产企业"
+        @confirm="supplyConfirm"
+      />
+      <scroll-view scroll-y v-else>
+        <view class="location-list">
+          <view
+            class="location"
+            :class="{ 'my-border-bottom': index < supplyList.length - 1 }"
+            v-for="(item, index) in supplyList"
+            :key="item.Id"
+            @click="select(item)"
+          >
+            <view class="name">
+              <view class="icon">
+                <uv-image
+                  src="/static/images/location.png"
+                  width="32rpx"
+                  height="32rpx"
+                  :duration="0"
+                />
+              </view>
+              {{ item.Name }}
+            </view>
+            <view class="address"
+              >{{ item.Province || "" }}{{ item.City || ""
+              }}{{ item.Address || "" }}</view
+            >
+            <view
+              class="dis"
+              v-if="
+                typeof getDis({
+                  latitude: item.Latitude,
+                  longitude: item.Logitude,
+                }) === 'number'
+              "
+              >距您
+              {{
+                getDis({ latitude: item.Latitude, longitude: item.Logitude })
+              }}
+              km</view
+            >
+          </view>
+        </view>
+        <view class="manage-address" @click="supplyConfirm">
+          查看全部生产企业
+          <uv-icon name="/static/images/arrow/green.png" size="28rpx" />
+        </view>
+        <uv-load-more status="nomore" color="#B0BECC" line-color="#B0BECC" />
+      </scroll-view>
+    </template>
+    <template v-if="type === 2">
+      <my-empty
+        v-if="isUnloadInit && unloadList.length === 0"
+        img="/static/images/empty/address2.png"
+        text="派单前需先维护卸货地址"
+        showButton
+        buttonText="新建卸货地址"
+        @confirm="unloadConfirm"
+      />
+      <scroll-view scroll-y v-else>
+        <view class="location-list">
+          <view
+            class="location"
+            :class="{ 'my-border-bottom': index < unloadList.length - 1 }"
+            v-for="(item, index) in unloadList"
+            :key="item.Id"
+            @click="select(item)"
+          >
+            <view class="name">
+              <view class="icon">
+                <uv-image
+                  src="/static/images/location.png"
+                  width="32rpx"
+                  height="32rpx"
+                  :duration="0"
+                />
+              </view>
+              {{ item.Placename }}
+            </view>
+            <view class="address"
+              >{{ item.Province || "" }}{{ item.City || ""
+              }}{{ item.District || "" }}{{ item.Address || "" }}</view
+            >
+            <view
+              class="dis"
+              v-if="
+                typeof getDis({
+                  latitude: item.Latitude,
+                  longitude: item.Logitude,
+                }) === 'number'
+              "
+              >距您
+              {{
+                getDis({ latitude: item.Latitude, longitude: item.Logitude })
+              }}
+              km</view
+            >
+          </view>
+        </view>
+        <view class="manage-address" @click="unloadConfirm">
+          管理卸货地址
+          <uv-icon name="/static/images/arrow/green.png" size="28rpx" />
+        </view>
+        <uv-load-more status="nomore" color="#B0BECC" line-color="#B0BECC" />
+      </scroll-view>
+    </template>
+  </view>
 </template>
 
 <script setup>
-	import { watchEffect, ref } from 'vue'
-	import { GetSupplierList, GetOwnerUnloadPlace } from '@/api/index.js'
-	import {
-		gcj02Distance
-	} from '@/utils/index.js'
-	import {
-		useLocationStore
-	} from '@/stores/location.js';
-	const locationStore = useLocationStore();
-	
-	const props = defineProps({
-		type: {
-			default: 1,
-		}
-	})
-	const emits = defineEmits(['change'])
+import { ref } from "vue";
+import { GetSupplierList, GetOwnerUnloadPlace } from "@/api/index.js";
+import { gcj02Distance } from "@/utils/index.js";
+import { useLocationStore } from "@/stores/location.js";
+const locationStore = useLocationStore();
+import { onPageShow } from "@dcloudio/uni-app";
 
-	const isInit1 = ref(false);
-	const isInit2 = ref(false);	
-	watchEffect(() => {
-		if(props.type === 1) {
-			getSupply();
-		}
-		if(props.type === 2) {
-			getUnload();
-		}
-	})
-	
-	const supplyList = ref([]);
-	const unloadList = ref([]);
-	// 获取装货地
-	async function getSupply() {
-		const res = await GetSupplierList();
-		// supplyList.value = res;
-		isInit1.value = true;
-	}
-	// 获取卸货地
-	async function getUnload() {
-		const res = await GetOwnerUnloadPlace({
-			useStatus: 1
-		})
-		unloadList.value = res.filter(m => m.EnabledMark === '1');
-		isInit2.value = true;
-	}
+const emits = defineEmits(["change"]);
 
-	function select(record) {
-		emits('change', {
-			type: props.type,
-			data: record
-		})
-	}
-	
-	
-	// 计算距离
-	function getDis({
-		latitude,
-		longitude
-	}) {
-		if (!locationStore.location || !latitude || !longitude) return null;
-		const distance = gcj02Distance({
-			lat1: latitude,
-			lon1: longitude, 
-			lat2: locationStore.location.latitude,
-			lon2: locationStore.location.longitude,
-		})
-		return distance;
-	}
+const type = ref(1);
+
+onPageShow(() => {
+  if (type.value === 1) {
+    getSupply();
+  }
+  if (type.value === 2) {
+    getUnload();
+  }
+});
+const isSupplyInit = ref(false);
+const isUnloadInit = ref(false);
+
+function supplyConfirm() {
+  uni.switchTab({
+    url: "/pages/enterprise/enterprise",
+  });
+}
+function unloadConfirm() {
+  uni.navigateTo({
+    url: "/pages/addressList/addressList",
+  });
+}
+const supplyList = ref([]);
+const unloadList = ref([]);
+// 获取装货地
+async function getSupply() {
+  const res = await GetSupplierList();
+  supplyList.value = res;
+  isSupplyInit.value = true;
+}
+// 获取卸货地
+async function getUnload() {
+  const res = await GetOwnerUnloadPlace({
+    useStatus: 1,
+  });
+  unloadList.value = res.filter((m) => m.EnabledMark === "1");
+  isUnloadInit.value = true;
+}
+
+function select(record) {
+  emits("change", {
+    type: type.value,
+    data: record,
+  });
+  close();
+}
+
+// 计算距离
+function getDis({ latitude, longitude }) {
+  if (!locationStore.location || !latitude || !longitude) return null;
+  const distance = gcj02Distance({
+    lat1: latitude,
+    lon1: longitude,
+    lat2: locationStore.location.latitude,
+    lon2: locationStore.location.longitude,
+  });
+  return distance;
+}
+
+const translateY = ref("100%");
+function open(config) {
+  type.value = config.type;
+  if (type.value === 1) {
+    getSupply();
+  }
+  if (type.value === 2) {
+    getUnload();
+  }
+
+  translateY.value = 0;
+}
+function close() {
+  translateY.value = "100%";
+}
+
+defineExpose({
+  open,
+  close,
+});
 </script>
 
 <style lang="scss" scoped>
-	.list-wrapper {
-		box-sizing: border-box;
-		height: 100vh;
-		padding: 24rpx;
-		background-color: var(--page-bg);
-		margin-bottom: 24rpx;
-	}
+.list-wrapper {
+  height: 100vh;
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 10000;
+  transform: translateY(100%);
+  transition: all 0.4s;
+  box-sizing: border-box;
+  height: 100vh;
+  padding: 24rpx;
+  background-color: var(--page-bg);
+  margin-bottom: 24rpx;
+  .scroll-view {
+    height: 100%;
+  }
+}
 
-	.location-list {
-		padding: 0 24rpx;
-		background-color: #ffffff;
-		border-radius: 24rpx;
-		overflow: hidden;
+.location-list {
+  padding: 0 24rpx;
+  background-color: #ffffff;
+  border-radius: 24rpx;
+  overflow: hidden;
 
-		.location {
-			padding: 32rpx 0;
+  .location {
+    padding: 32rpx 0;
 
-			.name,
-			.address {
-				margin-bottom: 16rpx;
-			}
+    .name,
+    .address {
+      margin-bottom: 16rpx;
+    }
 
-			.name,
-			.address,
-			.dis {
-				padding-left: 48rpx;
-			}
+    .name,
+    .address,
+    .dis {
+      padding-left: 48rpx;
+    }
 
-			.name {
-				position: relative;
-				font-weight: bold;
-				font-size: 32rpx;
-				color: var(--title-color);
-				line-height: 40rpx;
+    .name {
+      position: relative;
+      font-weight: bold;
+      font-size: 32rpx;
+      color: var(--title-color);
+      line-height: 40rpx;
 
-				.icon {
-					position: absolute;
-					top: 4rpx;
-					left: 0;
-				}
-			}
+      .icon {
+        position: absolute;
+        top: 4rpx;
+        left: 0;
+      }
+    }
 
-			.address {
-				font-size: 26rpx;
-				color: var(--sub-color);
-				line-height: 32rpx;
-			}
+    .address {
+      font-size: 26rpx;
+      color: var(--sub-color);
+      line-height: 32rpx;
+    }
 
-			.dis {
-				font-size: 26rpx;
-				color: var(--main-color);
-				line-height: 40rpx;
-			}
-		}
-	}
+    .dis {
+      font-size: 26rpx;
+      color: var(--main-color);
+      line-height: 40rpx;
+    }
+  }
+}
+
+.manage-address {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 26rpx;
+  color: var(--main-color);
+  margin: 20rpx 0 34rpx;
+  line-height: 1;
+  font-weight: bold;
+}
 </style>
